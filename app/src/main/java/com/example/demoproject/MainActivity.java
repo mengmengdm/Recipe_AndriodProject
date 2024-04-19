7d2ba15a385c0d1e90ec7b518f277ebc7511ae2d
Haoran Fang <fhr77382@qq.com>
2024-04-20 01:42:38 +0200

    add some fragments but nothing inside

--- app/src/main/java/com/example/demoproject/MainActivity.java
+++ app/src/main/java/com/example/demoproject/MainActivity.java
@@ -4,8 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
+    private BottomNavigationView bottomNavigationView;
+    private DiscoverFragment discoverFragment;
+    private MyRecipeFragment myRecipeFragment;
+    private PostFragment postFragment;
+    private ProfileFragment profileFragment;
+    private TodayFragment todayFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
