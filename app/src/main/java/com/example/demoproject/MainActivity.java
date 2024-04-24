147fd60a6a9d20a03d0fc9453fe2780463a3341d
ryan <1197539804@qq.com>
2024-04-24 17:18:12 +0200

    successfully switch between fragments by the navigation bar

--- app/src/main/java/com/example/demoproject/MainActivity.java
+++ app/src/main/java/com/example/demoproject/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.demoproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private BottomNavigationView bottomNavigationView;
     private DiscoverFragment discoverFragment;
     private MyRecipeFragment myRecipeFragment;
     private PostFragment postFragment;
@@ -18,5 +19,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        NavController navController = Navigation.findNavController(this, R.id.fragment_container);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
